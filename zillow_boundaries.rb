582ad9fba21db39a8b0d60600645efe2feb52340
sungry <sbchoi86@gmail.com>
2015-09-23 12:01:56 -0700

    add more comments

--- zillow_boundaries.rb
+++ zillow_boundaries.rb
@@ -1,6 +1,10 @@
 =begin
 1. parse shapefile into geojson file by using GDAL (ogr2ogr -f GeoJSON -t_srs EPSG:4269 [filename].geojson [filename].shp) command line
-2. use ARGV to get a file with street addresses parsed
+2. use ARGV to get a file with street addresses parsed into json format recognizable by Geocoder
+3. extract coordinates of street address by using Geocoder.coordinates(arg) method
+4. find the range(min and max) of Zillow's coordinates and to see if the address' coordinates are within the range
+  - First, run a function to find matching latitude
+5. If neighborhood name is available with the coordinates, return the name,
 
 =end
 
